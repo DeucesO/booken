e60749f6fd266a886846814ef65bf2d228a71968
Oliver Kabi <oliver@Olivers-MacBook-Air.local>
2016-04-22 13:52:33 +0100

    Added jade bootstrap and updated pages

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
+var mongoose = require('mongoose');
+
 module.exports = {
     url : function (env) {
         if (env === 'development')
@@ -5,5 +7,40 @@ module.exports = {
             return 'mongodb://localhost:27017/booken-test'
         }
         return 'mongodb://localhost:27017/booken'
+    },
+    
+    connect : function (env) {
+        if (env === 'development')
+        {
+            connectDb(env);
+            dropCreate();
+        }
     }
+}
+function getUrl (env) {
+    if (env === 'development')
+    {
+        return 'mongodb://localhost:27017/booken-test'
+    }
+    return 'mongodb://localhost:27017/booken'
+}
+
+function connectDb (env) {
+    mongoose.connect(getUrl(env));
+}
+
+function dropCreate () {
+    mongoose.connection.on('open', function () {
+        mongoose.connection.db.dropDatabase(function (err) {
+            console.log('booken-test database dropped');
+            seed();
+        })
+    });
+}
+
+function seed () {
+    var Account = require('../app/models/account');
+    Account.register(new Account({ username: 'admin'}), 'admin', function () {
+        console.log('admin account created');
+    });
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var path = require('path');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var session = require('express-session')
 
 var mongoose = require('mongoose');
 var passport = require('passport');
@@ -10,19 +11,25 @@ var passport = require('passport');
 var app = express();
 
 var configDB = require('./config/database');
-mongoose.connect(configDB.url(app.get('env')));
+configDB.connect(app.get('env'));
 
 require('./config/passport')(passport);
 
 // view engine setup
 app.set('views',path.join(__dirname, '/public/views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 app.use(methodOverride());
+
+app.use(session({ 
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: false
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
